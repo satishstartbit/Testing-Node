c58d2659bbb59c57f3e3ceb2ba66d929b7f6497d
jai <jai@ouverture.ai>
2025-03-28 16:36:03 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,10 +1,7 @@
 // server.js
 const express = require('express');
-const mongoose = require("mongoose");
-
 const app = express();
-const PORT = process.env.PORT || 3000;
-const DB_URL = process.env.DATABASE_URL;
+const port = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -16,16 +13,8 @@ app.get('/', (req, res) => {
     res.send('Hello, welcome to the Node.js Express app deployed on Vercel!');
 });
 
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+});
 
 
-mongoose
-    .connect(DB_URL)
-    .then(() => {
-        app.listen(PORT, () => {
-            console.log(`🚀 Server running on http://localhost:${PORT}`);
-        });
-    })
-    .catch((err) => {
-        console.error("❌ Failed to connect to MongoDB", err);
-        process.exit(1);
-    });
