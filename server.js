cb53277bae2c71219654ffe95a2d0f4e3be03b3f
jai <jai@ouverture.ai>
2025-03-28 16:32:05 +0530

    Update server.js

--- server.js
+++ server.js
@@ -3,53 +3,25 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 require("dotenv").config();
-const userRoutes = require("./routes/webportal/userRoutes"); // Your user routes
-const roleRoutes = require("./routes/webportal/roleRoutes"); // Your user routes
-const companyRoutes = require("./routes/webportal/companyRoutes"); // Your user routes
-
-const MobileRoutes = require("./routes/nativeapp/userRoutes");
 
 
 
 const app = express();
 
 const PORT = process.env.PORT || 3000;
-const DB_URL = process.env.DATABASE_URL;
+
 
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.use((req, res, next) => {
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    res.setHeader(
-        "Access-Control-Allow-Methods",
-        "OPTIONS, GET, POST, PUT, PATCH, DELETE"
-    );
-    res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
-    next();
-});
+
 
 
 app.get('/', (req, res) => {
     res.send('Hello, welcome to the Node.js Express app deployed on Vercel!');
 });
 
-app.use("/api/users", userRoutes);
-app.use("/api/role", roleRoutes);
-app.use("/api/company", companyRoutes);
-
-
-app.use("/mobile", MobileRoutes)
-
-
-app.get('/', (req, res) => {
-    res.send('Hello, World!');
-});
-
-app.get('/api/data', (req, res) => {
-    res.json({ message: "This is a sample API response" });
-});
 
 
 app.use((error, req, res, next) => {
@@ -60,14 +32,6 @@ app.use((error, req, res, next) => {
     });
 });
 
-mongoose
-    .connect(DB_URL)
-    .then(() => {
-        app.listen(PORT, () => {
-            console.log(`🚀 Server running on http://localhost:${PORT}`);
-        });
-    })
-    .catch((err) => {
-        console.error("❌ Failed to connect to MongoDB", err);
-        process.exit(1);
-    });
+app.listen(PORT, () => {
+    console.log(`🚀 Server running on http://localhost:${PORT}`);
+});
