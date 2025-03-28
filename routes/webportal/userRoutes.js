a63ff6c05bf85696091b25f3cd25db58bc9db8ca
jai <jai@ouverture.ai>
2025-03-28 16:54:24 +0530

    deplot

--- routes/webportal/userRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const { getAllUsers } = require('../../controllers/webportal/user/CreateUsers');
-
-const router = express.Router();
-
-// GET route to fetch all users
-router.get('/', getAllUsers);
-
-module.exports = router;
