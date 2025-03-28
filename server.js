c580fbf37dceb6d6978fa6c65733bf026d14d035
jai <jai@ouverture.ai>
2025-03-28 18:23:17 +0530

    feploy

--- server.js
+++ server.js
@@ -1,10 +1,8 @@
 const express = require('express');
 const mongoose = require("mongoose");
 const userRoutes = require("./routes/webportal/userRoutes"); // Your user routes
-const roleRoutes = require("./routes/webportal/roleRoutes"); // Your user routes
-const companyRoutes = require("./routes/webportal/companyRoutes"); // Your user routes
 
-const MobileRoutes = require("./routes/nativeapp/userRoutes");
+
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -13,11 +11,9 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use("/api/users", userRoutes);
-app.use("/api/role", roleRoutes);
-app.use("/api/company", companyRoutes);
 
 
-app.use("/mobile", MobileRoutes)
+
 
 app.get('/', (req, res) => {
     res.send('Hello, welcome to the Node.js Express app deployed on Vercel!');
