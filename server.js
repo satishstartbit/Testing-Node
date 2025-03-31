a6f577e96d9b50076485bcc9f259c0f2b182038c
jai <jai@ouverture.ai>
2025-03-31 10:40:43 +0530

    testing

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const mongoose = require("mongoose");
+
 const userRoutes = require("./routes/webportal/userRoutes"); // Your user routes
+const roleRoutes = require("./routes/webportal/roleRoutes"); // Your user routes
+const companyRoutes = require("./routes/webportal/companyRoutes"); // Your user routes
+const MobileRoutes = require("./routes/nativeapp/userRoutes");
 
 
 
@@ -10,9 +14,11 @@ const PORT = process.env.PORT || 3000;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+
 app.use("/api/users", userRoutes);
-
-
+app.use("/api/role", roleRoutes);
+app.use("/api/company", companyRoutes);
+app.use("/mobile", MobileRoutes)
 
 
 app.get('/', (req, res) => {
